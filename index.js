bfc008db818bc09cd07a5a8e05d50fa8b4227d86
Wamuyu <michellewachira25@gmail.com>
2022-06-27 12:36:39 +0300

    fixing small issues

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-//Updating the DOM
+//loading the DOM
+document.addEventListener("DOMContentLoaded", () => {
 
-//declaring Variables
-const searchForm = document.querySelector("form");
-const searchResultDiv = document.querySelector(".item");
-const container = document.querySelector(".container");
-let searchQuery = parseInt("");
-
-
-//making the event listener - functioning
-searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    searchQuery = e.target.querySelector("input").value;
-    fetchAPI(searchQuery);
-});
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '60a0fdf920msh68972d9786b7056p16a89fjsnca2e13235a8f',
-        'X-RapidAPI-Host': 'burgers1.p.rapidapi.com'
-    }
-};
-
-//loading the API - done
-async function fetchAPI(id) {
-    let baseUrl = `https://burgers1.p.rapidapi.com/burgers/${id}`;
-    const response = await fetch(baseUrl, options);
-    let data = await response.json();
-    generateHTML(data);
-};
+    //declaring Variables
+    const searchForm = document.querySelector("form");
+    const searchResultDiv = document.querySelector(".item");
+    const container = document.querySelector(".container");
+    let searchQuery = parseInt("");
 
 
-//generating the results on the web
-function generateHTML(results) {
-    container.classList.remove("initial");
-    let generatedHTML = "";
-    let result = Object.values(results);
-    console.log(result)
-    for (i = 0; i < result.length; i++) {
-        generatedHTML = `
+    //making the event listener - functioning
+    searchForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        searchQuery = e.target.querySelector("input").value;
+        fetchAPI(searchQuery);
+    });
+
+    //loading the API - done
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '60a0fdf920msh68972d9786b7056p16a89fjsnca2e13235a8f',
+            'X-RapidAPI-Host': 'burgers1.p.rapidapi.com'
+        }
+    };
+
+    async function fetchAPI(id) {
+        let baseUrl = `https://burgers1.p.rapidapi.com/burgers/${id}`;
+        const response = await fetch(baseUrl, options);
+        let data = await response.json();
+        generateHTML(data);
+    };
+
+
+    //generating the results on the web
+    function generateHTML(results) {
+        container.classList.remove("initial");
+        let generatedHTML = "";
+        let result = Object.values(results);
+        console.log(result)
+        for (i = 0; i < result.length; i++) {
+            generatedHTML = `
              <div class = "item" >
             <img src = "amirali-mirhashemian-sc5sTPMrVfk-unsplash.jpg"alt = "" >
         <div class = "flex-container" >
@@ -50,35 +52,37 @@ function generateHTML(results) {
      <p></p>
      <div>
                                 `
-    };
-    searchResultDiv.innerHTML = generatedHTML;
-}
-
-//making the like button(done)
-var btn1 = document.querySelector('#green');
-var btn2 = document.querySelector('#red');
-
-btn1.addEventListener('click', function() {
-
-    if (btn2.classList.contains('red')) {
-        btn2.classList.remove('red');
+        };
+        searchResultDiv.innerHTML = generatedHTML;
     }
-    this.classList.toggle('green');
 
-});
+    //making the like button(done)
+    var btn1 = document.querySelector('#green');
+    var btn2 = document.querySelector('#red');
 
-btn2.addEventListener('click', function() {
+    btn1.addEventListener('click', function() {
 
-    if (btn1.classList.contains('green')) {
-        btn1.classList.remove('green');
-    }
-    this.classList.toggle('red');
-    form.addEventListener('submit', (event) => {
+        if (btn2.classList.contains('red')) {
+            btn2.classList.remove('red');
+        }
+        this.classList.toggle('green');
+
+    });
+
+    btn2.addEventListener('click', function() {
+
+        if (btn1.classList.contains('green')) {
+            btn1.classList.remove('green');
+        }
+        this.classList.toggle('red');
 
     });
 
 
     //making the submit response on the contact us form(done)
     const form = document.getElementById("form-contact")
-    event.preventDefault();
-});
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    });
+
+})
